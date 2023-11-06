97c19f29ed9f9957d143d4651084c2587330aac0
victordemolini <victor.demolini@sarria.salesians.cat>
2023-11-06 23:29:21 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/entrada_dades/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.entrada_dades
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Recuperar el valor d''un camp de text',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recuperar el valor d\'un camp de text'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: myController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(myController.text),
+              duration: const Duration(seconds: 2),
+            ),
+          );
+        },
+        tooltip: 'Mostrar el valor',
+        child: const Icon(Icons.text_fields),
+      ),
+    );
+  }
+}
